46f527bbc438ae4679ef8dafb27bbbb749ede3c7
Nicolas734 <nichollaslimma@gmail.com>
2023-07-26 15:09:13 -0300

    criado switch seprado pro cliente e pro produto

--- main.c
+++ main.c
@@ -21,17 +21,19 @@ typedef struct{
 } Loja_t;
 
 
+void opcoesClientes(void);
+void opcoesProdutos(void);
+
+Loja_t loja;
+int qtd_clientes = 0;
+int qtd_produtos = 0;
+
 void main(void){
     int option;
-    Loja_t loja;
-    int qtd_clientes = 0;
-    int qtd_produtos = 0;
 
     for(;;){
-        printf("1 - Cadastrar Clientes\n");
-        printf("2 - Cadastrar Produtos\n");
-        printf("3 - Mostrar Clientes\n");
-        printf("4 - Mostrar Produtos\n");
+        printf("1 - Interface dos clientes\n");
+        printf("2 - Interface dos produtos\n");
         printf("0 - Sair\n");
 
         printf("Escolha uma opção: ");
@@ -40,53 +42,15 @@ void main(void){
         switch (option)
         {
         case 1:
-            Usuario_t cliente;
-            printf("\nEscreva o nome: ");
-            scanf("%s", cliente.nome);
-            printf("\nEscreva o seu email: ");
-            scanf("%s", cliente.email);
-            printf("\nEscreva a sua senha: ");
-            scanf("%s", cliente.senha);
-            memcpy(&loja.clientes[qtd_clientes], &cliente, sizeof(Usuario_t));
-            qtd_clientes++;
+            opcoesClientes();
             break;
 
         case 2:
-            Produto_t produto;
-            printf("\nEscreva o nome do produto: ");
-            scanf("%s", produto.nome);
-            printf("\nEscreva o preço do produto: ");
-            scanf("%d", &produto.preco);
-            memcpy(&loja.produtos[qtd_produtos], &produto, sizeof(Produto_t));
-            qtd_produtos++;
-            break;
-
-        case 3:
-            printf("\n*** Cleintes cadastrados no sistema ***\n");
-            if(qtd_clientes > 0){
-                for (int i = 0; i < qtd_clientes; i++) {
-                    printf("Nome: %s\n", loja.clientes[i].nome);
-                    printf("Email: %s\n", loja.clientes[i].email);
-                }
-            }else{
-                printf("\nNão possui clientes cadastrados\n");
-            }
-            break;
-
-        case 4:
-            printf("\n*** Produtos em estoque ***\n");
-            if(qtd_produtos > 0){
-                for(int i = 0; i < qtd_produtos; i++){
-                    printf("Nome: %s\n", loja.produtos[i].nome);
-                    printf("Preço: $ %d\n", loja.produtos[i].preco);
-                }
-            }else{
-                printf("\nNenhum produto em estoque\n");
-            }
+            opcoesProdutos();
             break;
 
         case 0:
-            printf("Saindo...");
+            printf("Saindo...\n");
             exit(0);
             break;
 
@@ -95,5 +59,93 @@ void main(void){
         }
     }
 
+}
 
+
+void opcoesClientes(void){
+    int option;
+
+    printf("1 - Cadastrar Clientes\n");
+    printf("2 - Listar Clientes\n");
+    printf("0 - Voltar\n");
+
+    printf("Escolha uma opção: ");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case 1:
+        Usuario_t cliente;
+        printf("\nEscreva o nome: ");
+        scanf("%s", cliente.nome);
+        printf("\nEscreva o seu email: ");
+        scanf("%s", cliente.email);
+        printf("\nEscreva a sua senha: ");
+        scanf("%s", cliente.senha);
+        memcpy(&loja.clientes[qtd_clientes], &cliente, sizeof(Usuario_t));
+        qtd_clientes++;
+        printf("%s", cliente.nome);
+        break;
+    
+    case 2:
+        printf("\n*** Cleintes cadastrados no sistema ***\n");
+        if(qtd_clientes > 0){
+            for (int i = 0; i < qtd_clientes; i++) {
+                printf("Nome: %s\n", loja.clientes[i].nome);
+                printf("Email: %s\n", loja.clientes[i].email);
+            }
+        }else{
+            printf("\nNão possui clientes cadastrados\n");
+        }
+        break;
+    
+    case 0:
+        return;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+
+void opcoesProdutos(){
+    int option;
+
+    printf("1 - Cadastrar Produtos\n");
+    printf("2 - Mostrar Produtos\n");
+    printf("0 - Voltar\n");
+
+    printf("Escolha uma opção: ");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case 1:
+        Produto_t produto;
+        printf("\nEscreva o nome do produto: ");
+        scanf("%s", produto.nome);
+        printf("\nEscreva o preço do produto: ");
+        scanf("%d", &produto.preco);
+        memcpy(&loja.produtos[qtd_produtos], &produto, sizeof(Produto_t));
+        qtd_produtos++;
+        break;
+    case 2:
+        printf("\n*** Produtos em estoque ***\n");
+        if(qtd_produtos > 0){
+            for(int i = 0; i < qtd_produtos; i++){
+                printf("Nome: %s\n", loja.produtos[i].nome);
+                printf("Preço: $ %d\n", loja.produtos[i].preco);
+            }
+        }else{
+            printf("\nNenhum produto em estoque\n");
+        }
+        break;
+    case 0:
+        return;
+        break;
+    default:
+        break;
+    }
 }
